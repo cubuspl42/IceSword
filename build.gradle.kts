22967820eee4ad5f7cd427ea2a2ac2897fc25a27
cubuspl42 <cubuspl42@gmail.com>
2021-07-16 11:15:56 +0200

    Add `pako` dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("js") version "1.5.20"
+    kotlin("js") version "1.5.21"
 }
 
 group = "me.kuba"
@@ -12,6 +12,10 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlin-wrappers:kotlin-css:1.0.0-pre.214-kotlin-1.5.20")
+
+    implementation(npm("pako", "2.0.3"))
+    implementation(npm("@types/pako", "1.0.2", generateExternals = true))
+
     testImplementation(kotlin("test"))
 }
 
