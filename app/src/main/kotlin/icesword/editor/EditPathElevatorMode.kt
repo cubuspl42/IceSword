ad64e3103b10b426d02d397a9e35462c9402a330
cubuspl42 <cubuspl42@gmail.com>
2021-11-19 16:36:44 +0100

    Snap path elevator step during moving

--- app/src/main/kotlin/icesword/editor/EditPathElevatorMode.kt
+++ app/src/main/kotlin/icesword/editor/EditPathElevatorMode.kt
@@ -7,6 +7,10 @@ import icesword.frp.Till
 import icesword.frp.Tilled
 import icesword.frp.map
 import icesword.geometry.IntVec2
+import kotlinx.css.pre
+import kotlin.math.absoluteValue
+import kotlin.math.max
+import kotlin.math.sign
 
 
 class EditPathElevatorMode(
@@ -33,16 +37,44 @@ class EditPathElevatorMode(
             val initialPosition = step.position.sample()
             val targetPosition = positionDelta.map { initialPosition + it }
 
+            val threshold = 20
+
+            val snappedPosition = targetPosition.map { tp ->
+                step.previous?.let { prevStep ->
+                    val pp = prevStep.position.sample()
+                    val d = (tp - pp)
+                    when {
+                        // Horizontal axis
+                        d.x.absoluteValue < threshold -> tp.copy(x = pp.x, y = tp.y)
+                        // Vertical axis
+                        d.y.absoluteValue < threshold -> tp.copy(x = tp.x, y = pp.y)
+                        // NW/SE axis
+                        (d.x - d.y).absoluteValue < threshold -> {
+                            val a = max(d.x, d.y)
+                            pp + IntVec2(x = a, y = a)
+                        }
+                        // NE/SW axis
+                        (d.x + d.y).absoluteValue < threshold -> {
+                            val sx = if (d.x >= 0) 1 else -1
+                            val sy = if (d.y >= 0) 1 else -1
+                            val a = max(d.x.absoluteValue, d.y.absoluteValue)
+                            pp + IntVec2(x = sx * a, y = sy * a)
+                        }
+                        else -> tp
+                    }
+                } ?: tp
+            }
+
             _enterMoveStepMode.send(
                 object : Tilled<MoveStepMode> {
                     override fun build(till: Till): MoveStepMode {
                         step.moveTo(
-                            globalPosition = targetPosition,
+                            globalPosition = snappedPosition,
                             tillStop = till,
                         )
 
                         return MoveStepMode(
-                            targetPosition = targetPosition,
+                            targetPosition = snappedPosition,
                             tillStop = tillStop,
                         )
                     }
