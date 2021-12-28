791d12210f0b38011282816e736d0347d025af95
cubuspl42 <cubuspl42@gmail.com>
2021-12-28 15:34:57 +0100

    Upgrade Kotlin JS plugin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("js") version "1.5.31" apply false
+    kotlin("js") version "1.6.10" apply false
     kotlin("jvm") version "1.6.10" apply false
 }
 
@@ -10,12 +10,5 @@ allprojects {
     repositories {
         jcenter()
         mavenCentral()
-    }
-}
-
-// Workaround for https://youtrack.jetbrains.com/issue/KT-49124
-rootProject.plugins.withType<org.jetbrains.kotlin.gradle.targets.js.yarn.YarnPlugin> {
-    rootProject.the<org.jetbrains.kotlin.gradle.targets.js.yarn.YarnRootExtension>().apply {
-        resolution("@webpack-cli/serve", "1.5.2")
     }
 }
